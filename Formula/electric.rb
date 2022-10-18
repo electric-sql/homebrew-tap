5175e3da4ff2f9c9d7ea8ad7101448d890422225
Ilia Borovitinov <icehaunter@gmail.com>
2022-10-18 11:04:32 +0000

    electric: add 0.0.1-initial bottle.

--- Formula/electric.rb
+++ Formula/electric.rb
@@ -6,6 +6,12 @@ class Electric < Formula
   sha256 "1a7f0013c5d5a7911df0ad8db2cec173570601761e74f3d72b9ad2b2cf2fd58f"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/electric-sql/tap"
+    sha256                               monterey:     "95773c3b5e5e59d4285815c7e359dc1c67fbbb2a5716afd3752a0373a7296a29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5d3031873b7f7e45eccb7ee8e23a80049d36175022cf05d057ea55de78ba18c9"
+  end
+
   depends_on "elixir"
   depends_on "erlang"
 
