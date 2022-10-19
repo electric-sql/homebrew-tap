a68de3760d9dcd07881ff6a7c9613f7b8b60cf0e
Ilia Borovitinov <ilia@vaxine.io>
2022-10-19 11:39:35 +0300

    Fixed homepage

--- Formula/electric.rb
+++ Formula/electric.rb
@@ -1,6 +1,6 @@
 class Electric < Formula
   desc "CLI tool to interact with ElectricSQL cluster"
-  homepage "https://brew.sh"
+  homepage "https://electric-sql.com"
   url "https://github.com/electric-sql/cli/archive/refs/tags/v0.0.1-initial.tar.gz"
   version "0.0.1-initial-m1"
   sha256 "1a7f0013c5d5a7911df0ad8db2cec173570601761e74f3d72b9ad2b2cf2fd58f"
