091db473f6a04f8bb9bdabdd642a235cb26a6545
Ilia Borovitinov <icehaunter@gmail.com>
2023-01-09 19:35:13 +0000

    electric: update 0.2.1 bottle.

--- Formula/electric.rb
+++ Formula/electric.rb
@@ -7,10 +7,10 @@ class Electric < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/electric-sql/tap"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "0c3cd6f9c976981c9aefc7d94eb439f1ab5f6e6720c1c31f08ce19688acb7130"
-    sha256 cellar: :any_skip_relocation, monterey:       "280db3791eaebba0fa1177b721b90ca56ee76d37f4cff973b5ef980a0822cff3"
-    sha256 cellar: :any_skip_relocation, big_sur:        "12fcf7e3c5e251d619123d0fc8c20c14f3600a678eb8fa3cf769f6fcdc23827e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "0ff064652a9cde59393f1d0ad497a19fb6a4df89c2d7950e71bdccbe8a77824e"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "278d403b663d40258d42af0d18aabe79f42a8ec9bb8fc1d3765b406ba7d74fda"
+    sha256 cellar: :any_skip_relocation, monterey:       "88e7d0494208b0913c9f8d0a38963ab5e1a2852e3c7cca16959eb3d8bf792535"
+    sha256 cellar: :any_skip_relocation, big_sur:        "0bd844e8addf23cac822f4b82c96e81610895247b2cec0a4c9b4059618675e13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "2119c520da7eec8195e6b54c7e3bd8d76e03dddd17cee216d40df94588dbacc5"
   end
 
   depends_on "elixir" => :build
